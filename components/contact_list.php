5ff235cdbad73c146cdf32ded79d84011eb75e22
Natasha Gorell <natasha.gorell@connect.qut.edu.au>
2015-09-07 13:50:13 +1000

    Added contact us page

---
+++ components/contact_list.php
@@ -0,0 +1,25 @@
+<html> <font face="Helvetica">
+	<head>
+		<title>Contact Us</title>
+	</head>
+
+<p align='center'><font size='18'><b>Contact Us</b></font>
+<style>
+    body {
+      color: black;
+    }
+  </style>
+  <body bgcolor='#006699' topmargin='50' leftmargin='50'> 
+	<p><img src="map.jpg" alt="Map" align='right'> 
+	<h2>Office</h2>
+	<p><b>Hours:</b> 9:00am - 5:00pm Monday to Friday     
+	<br><b>Address:</b> 8 Shore St W, Ormiston QLD 4160
+	<br><b>Contact Number:</b> 07 3386 5719
+	<br><b>Email:</b> david@.....
+
+
+
+<p><br><p><br><p><br><p> <br><p> <br><p align='center'><a href="http://localhost/PM/">Home</a>
+  </body>
+</font>
+</html>
